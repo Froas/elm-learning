8595371d92fb74f5cdda06f703349efb1a071a0c
froas <froas.crp@gmail.com>
2021-06-25 20:20:48 +0600

    my first commit

---
+++ src/MarginalSquare.elm
@@ -0,0 +1,15 @@
+module MarginalSquare exposing(..)
+
+import Browser
+import Html exposing (Html, div)
+import Main exposing (Model)
+
+
+
+
+view : Model -> Html ()
+view model =
+    div []
+        [
+
+        ]
